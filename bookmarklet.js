7d7e776446801dce6f36eacca2b612fd5337a1d8
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2012-04-17 10:44:47 +0800

    set jquery in noconflict mode
    if someone is using prototype
    and hits the bookmark
    we dont want to break $

--- bookmarklet.js
+++ bookmarklet.js
@@ -7,7 +7,11 @@
 if (typeof jQuery == 'undefined') {
 	var jQ = document.createElement('script');
 	jQ.type = 'text/javascript';
-	jQ.onload=runthis;
+	jQ.onload=function(){
+		// avoid breaking other project projects using $
+		jQuery.noConflict();
+		runthis();
+	};
 	jQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
 	document.body.appendChild(jQ);
 }
